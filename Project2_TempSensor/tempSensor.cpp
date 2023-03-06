930eaf2ed5c41251509ddf8f2a62a304b21dddbf
jaykempis <107106232+jaykempis@users.noreply.github.com>
2023-03-06 23:06:55 +0800

    Updated R(T) calculator

--- Project2_TempSensor/tempSensor.cpp
+++ Project2_TempSensor/tempSensor.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <math.h>
+#include <sstream>
 
 
 using namespace std;
@@ -10,8 +11,7 @@ double solveBeta(){
     double beta;
     double kelvin0 = 273.15, kelvin25 = 298.15;
     beta = (log(3))/((1/kelvin0)-(1/kelvin25));
-    cout <<"Beta: " <<beta << endl;
-
+    //cout <<"Beta: " <<beta << endl;
     return beta;
 }
 
@@ -19,24 +19,58 @@ double rNull(double tnull){
     double rnull, powerFactor;
     double kelvin0 = 273.15;
     powerFactor = solveBeta() * ((1/kelvin0)-(1/tnull));
-    rnull = (3000) / (exp(powerFactor));
-    cout <<"Ro: " <<rnull << endl;
+    rnull = (3000.0) / (exp(powerFactor));
+    //cout <<"Ro: " <<rnull << endl;
     return rnull;
 }
 
 double rTemp(double tempIn, double tnull){
     double rT;
-    double kelvinO = 273.15;
-    double powerFactor = solveBeta() * ((1/tempIn)-(1/tnull));
-    rT = rNull(tnull)*exp(powerFactor);
-    cout <<"PowerFactor: " <<powerFactor << endl;
-    cout << rT << endl;
+    double kelvinO = 273.1500;
+    double powerFactor = solveBeta()*((1.000/tempIn)-(1.000/tnull))*1.000;
+    rT = rNull(tnull)*1.0000*exp(powerFactor);
+    //cout <<"PowerFactor: " <<powerFactor << endl;
+    //cout <<"Ro: " <<rNull(tnull) << endl;
+    cout << "R(" << tempIn << ")= "<<rT <<endl;
     return rT;
 }
 
+void helpCMD(){
+    cout << "Insert Help dialogue" << endl;
+}
+
 int main(){
-    
+    string cmd;
+
+    float tempIn;
     double tnull = 293.15;
-    cout << rTemp(228.15,tnull) << endl;
+
+    do{
+        cout<<"\n[CMD] : ";
+        getline(cin, cmd);
+
+        stringstream cc(cmd);
+
+        if (cmd == "help")
+        {
+            helpCMD();
+        }
+        else if (cc >> tempIn)
+        { 
+            cout << "T = " << stod(cmd) << " K\n";
+            cout << "To = " << tnull << " K\n";
+            cout << "Ro = " << rNull(tnull) << endl;
+            cout << "β = " << solveBeta() << endl;
+            cout << "==========================\n";
+            rTemp(stod(cmd),tnull); 
+        }
+        else if (cmd == "exit")
+        {
+            cout << "Exiting..." << endl;
+        }
+        else cout<<"Invalid Command. " <<  
+        "Type 'help' for a list of commands."<<endl;
+    }
+    while (cmd != "exit");
 
 }
